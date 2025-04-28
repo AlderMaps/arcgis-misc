7f093a5a4ff9219574ad3c4d86725d6013438a49
Alder Maps <45706586+AlderMaps@users.noreply.github.com>
2025-04-28 12:09:41 -0700

    Update popup_populated_fields_only.js
    Completely re-done to split the address on top from the table on the bottom (table is usually-not-populated attributes). Also did a custom table instead of the built-in version like the example scripts had.

--- popup_populated_fields_only.js
+++ popup_populated_fields_only.js
@@ -1,10 +1,19 @@
+Expects($feature, '*')
+var fields = Schema($feature).fields
 
-var popup_fields = [
-    'USER_listing_name',
-    'StAddr',
-    'City',
-    'RegionAbbr',
-    'Postal',
+// For address, which all features should have
+
+var addr_arr = []
+
+Push(addr_arr, '<strong>' + $feature['StAddr'])
+Push(addr_arr, $feature['City'] + ', ' + $feature['RegionAbbr'] + ' ' + $feature['Postal'] + '</strong>')
+
+var addr_text = Concatenate(addr_arr, '<br>')
+var popup_text = addr_text
+
+// For table of sometimes-populated attributes
+
+var table_fields = [
     'USER_orgnization',
     'USER_listing_desc',
     'USER_location_site',
@@ -20,31 +29,33 @@ var popup_fields = [
     'USER_FNAP'
 ]
 
-Expects($feature, '*')
-
-var popup_arr = []
-
-var fields = Schema($feature).fields
+var table_text = ''
+var table_arr = []
 
 for (var f of $feature) {
-    if (Includes(popup_fields, f.key)) {
+    if (Includes(table_fields, f.key)) {
         if (!IsEmpty(f.value)) {
-
-            // Thanks to this post for help with this part:
-            // https://community.esri.com/t5/arcgis-online-questions/don-t-show-null-values-in-popup/m-p/1359714/highlight/true#M56367
             var alias = f.key
             for (var n in fields) {
                 if (fields[n].name == f.key) alias = fields[n].alias
             }
-
-            Push(popup_arr, alias + ': <b>' + f.value + '</b>')
+			Push(table_arr, '<tr><td style="border: 1px solid #ccc; padding: 5px;">' + alias)
+            Push(table_arr, '</td><td style="border: 1px solid #ccc; padding: 5px;">' + f.value)
+            Push(table_arr, '</td></tr>')
         }
     }
 }
 
-var popup_text = Concatenate(popup_arr, '<br>')
+Console(table_arr)
+
+if (Count(table_arr) > 0) {
+    Insert(table_arr, 0, '<table style="font-size: 8pt; line-height: 11pt; border-collapse: collapse;">')
+    Push(table_arr, '</table>')
+    table_text = Concatenate(table_arr)
+    popup_text = Concatenate(addr_text, '<br><br>', table_text)
+}
 
 return {
     type: 'text',
-    text: `<p>${popup_text}</p>`
+    text: popup_text
 }
